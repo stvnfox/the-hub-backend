0acf4e5425c9d112c7bf0f8753523e0f5acf27e4
Steven <steven@stvnx.nl>
2024-03-27 13:32:44 +0100

    chore: check if ts-ignore is still necessary

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.setGlobalPrefix("/api")
     app.use(
-        // @ts-ignore
         session({
             secret: process.env.SESSION_SECRET,
             saveUninitialized: false,
